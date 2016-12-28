44afa7b4cb6d3f8188fda98178a29b477929a8ec
Florent Castelli <florent.castelli@gmail.com>
2016-12-28 18:12:46 +0100

    Add wave library

---
+++ libs/wave.cmake
@@ -0,0 +1,19 @@
+_add_boost_lib(
+  NAME wave
+  SOURCES
+    ${BOOST_SOURCE}/libs/wave/src/cpplexer/re2clex/aq.cpp
+    ${BOOST_SOURCE}/libs/wave/src/cpplexer/re2clex/cpp_re.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_cpp_exprgrammar.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_cpp_grammar.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_cpp_literalgrs.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_defined_grammar.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_predef_macros.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_re2c_lexer.cpp
+    ${BOOST_SOURCE}/libs/wave/src/instantiate_re2c_lexer_str.cpp
+    ${BOOST_SOURCE}/libs/wave/src/token_ids.cpp
+    ${BOOST_SOURCE}/libs/wave/src/wave_config_constant.cpp
+  LINK
+    Boost::date_time
+    Boost::filesystem
+    Boost::thread
+)
