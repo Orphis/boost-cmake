05f3566d5bdc3952a1e82a4dc8be7573973dac03
Florent Castelli <florent.castelli@gmail.com>
2017-01-01 12:27:13 +0100

    Add atomic tests

--- libs/atomic.cmake
+++ libs/atomic.cmake
@@ -6,3 +6,20 @@ _add_boost_lib(
     BOOST_ATOMIC_STATIC_LINK=1
     BOOST_ATOMIC_SOURCE
 )
+
+_add_boost_test(
+  NAME atomic_test
+  LINK
+    Boost::atomic
+    Boost::thread
+  DEFINE
+    # The tests expect APIs deprecated in Thread v4, so enable them
+    BOOST_THREAD_PROVIDES_NESTED_LOCKS=1
+    BOOST_THREAD_USES_DATETIME=1
+  TESTS
+    RUN ${BOOST_SOURCE}/libs/atomic/test/native_api.cpp
+    RUN ${BOOST_SOURCE}/libs/atomic/test/fallback_api.cpp
+    RUN ${BOOST_SOURCE}/libs/atomic/test/atomicity.cpp
+    RUN ${BOOST_SOURCE}/libs/atomic/test/ordering.cpp
+    RUN ${BOOST_SOURCE}/libs/atomic/test/lockfree.cpp
+)
