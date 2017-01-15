8315a1171851bd4c7288aecb587cac8ba77e0c09
Florent Castelli <florent.castelli@gmail.com>
2017-01-15 22:13:22 +0100

    Fix ccache

--- cmake/Modules/ccache.cmake
+++ cmake/Modules/ccache.cmake
@@ -1,7 +1,7 @@
 find_program(CCACHE_BIN ccache)
 if(CCACHE_BIN)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_FOUND})
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_FOUND})
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_BIN})
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ${CCACHE_BIN})
 
     # ccache uses -I when compiling without preprocessor, which makes clang complain.
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
