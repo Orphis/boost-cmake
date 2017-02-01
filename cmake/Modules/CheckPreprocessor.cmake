8fc511facda20218eaa558294e03ca9008fbf85e
Florent Castelli <florent.castelli@gmail.com>
2017-02-01 19:56:03 +0100

    Use check_cxx_symbol_exists in check_preprocessor

--- cmake/Modules/CheckPreprocessor.cmake
+++ cmake/Modules/CheckPreprocessor.cmake
@@ -1,31 +1,10 @@
+include(CheckCXXSymbolExists)
+
 function(check_preprocessor output_variable symbol)
-  if(DEFINED _check_preprocessor_cache_${symbol})
-    set(${output_variable} "${_check_preprocessor_cache_${symbol}}" PARENT_SCOPE)
-    return()
+  set(CMAKE_REQUIRED_QUIET OFF)
+  check_cxx_symbol_exists(${symbol} "" ${output_variable})
+  if(NOT ${output_variable})
+    set(${output_variable} 0 CACHE INTERNAL "Have symbol ${symbol}" FORCE)
   endif()
-
-  enable_language(C)
-
-set(_check_preprocessor_source "
-#if defined(${symbol})
-int main() { return 0; }
-#endif
-")
-
-  set(_check_preprocessor_source_name "${CMAKE_CURRENT_BINARY_DIR}/check_preprocessor.c")
-  file(WRITE "${_check_preprocessor_source_name}" "${_check_preprocessor_source}")
-
-  try_compile(_check_preprocessor_result "${CMAKE_BINARY_DIR}" "${_check_preprocessor_source_name}")
-  file(REMOVE "${_check_preprocessor_source_name}")
-
-
-  if(_check_preprocessor_result)
-    set(_check_preprocessor_cache_${symbol} TRUE CACHE BOOL "" FORCE)
-  else()
-    set(_check_preprocessor_cache_${symbol} FALSE CACHE BOOL "" FORCE)
-  endif()
-  mark_as_advanced(_check_preprocessor_cache_${symbol})
-
-  set(${output_variable} "${_check_preprocessor_cache_${symbol}}" PARENT_SCOPE)
 endfunction()
 
