3ae7309c8067f87706456efd20607fdad7103255
Tijme Gommers <tijme@finnwea.com>
2017-03-16 22:24:22 +0100

    Fixed invalid reference to parallel source file.

--- libs/graph_parallel.cmake
+++ libs/graph_parallel.cmake
@@ -6,7 +6,7 @@ _add_boost_lib(
   NAME graph_parallel
   SOURCES
     ${BOOST_SOURCE}/libs/graph_parallel/src/mpi_process_group.cpp
-    ${BOOST_SOURCE}/libs/graph_parallel/src/allocator.cpp
+    ${BOOST_SOURCE}/libs/graph_parallel/src/tag_allocator.cpp
   LINK
     Boost::mpi
 )
