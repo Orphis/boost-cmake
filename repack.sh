115deaba3648dad5b3e816094d0a7b6ec693fb9e
Florent Castelli <florent.castelli@gmail.com>
2017-04-22 18:26:15 +0200

    Point repack script to 1.64.0

--- repack.sh
+++ repack.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BOOST_VERSION=1.63.0
+BOOST_VERSION=1.64.0
 
 function finish {
   rm -rf ${tmp_dir}
@@ -14,7 +14,7 @@ patch_dir=$(pwd)/patch/${BOOST_VERSION}
 tmp_dir=$(mktemp -d)
 
 echo "Downloading Boost ${BOOST_VERSION}..."
-curl -L "http://downloads.sourceforge.net/project/boost/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//./_}.tar.bz2" > ${tmp_dir}/boost_${BOOST_VERSION}.tar.bz2
+curl -L "https://dl.bintray.com/boostorg/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION//\./_}.tar.bz2" > ${tmp_dir}/boost_${BOOST_VERSION}.tar.bz2
 
 mkdir -p ${tmp_dir}/extract
 cd ${tmp_dir}/extract
@@ -22,12 +22,15 @@ echo "Extracting archive..."
 tar xf ${tmp_dir}/boost_${BOOST_VERSION}.tar.bz2
 
 cd boost_*
-mkdir patch
-for f in ${patch_dir}/*.patch; do
-  echo "Applying patch ${f}..."
-  git apply --verbose $f
-  cp $f patch/
-done
+
+if [ -d "${patch_dir}" ]; then
+  mkdir patch
+  for f in ${patch_dir}/*.patch; do
+    echo "Applying patch ${f}..."
+    git apply --verbose $f
+    cp $f patch/
+  done
+fi
 
 echo "Removing extra files..."
 find . -name "doc" -print0 | xargs -0 -- rm -rf
