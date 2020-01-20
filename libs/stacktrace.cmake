fe9f264157ec470c3cb60b956c5f0e18a0465e4c
André Rocha <d3roch4@gmail.com>
2020-01-20 14:14:25 -0300

    atualizando

--- libs/stacktrace.cmake
+++ libs/stacktrace.cmake
@@ -1,12 +1,38 @@
 _add_boost_lib(
-  NAME stacktrace
+  NAME stacktrace_addr2line
   SOURCES
     ${BOOST_SOURCE}/libs/stacktrace/src/addr2line.cpp
-    ${BOOST_SOURCE}/libs/stacktrace/src/backtrace.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/backtrace.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/basic.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/noop.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/windbg.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/windbg_cached.cpp
+  DEFINE_PRIVATE
+    BOOST_STACKTRACE_STATIC_LINK=1
+)
+
+_add_boost_lib(
+  NAME stacktrace_basic
+  SOURCES
+#    ${BOOST_SOURCE}/libs/stacktrace/src/addr2line.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/backtrace.cpp
     ${BOOST_SOURCE}/libs/stacktrace/src/basic.cpp
-    ${BOOST_SOURCE}/libs/stacktrace/src/noop.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/noop.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/windbg.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/windbg_cached.cpp
+  DEFINE_PRIVATE
+    BOOST_STACKTRACE_STATIC_LINK=1
+)
+
+_add_boost_lib(
+  NAME stacktrace_windbg
+  SOURCES
+#    ${BOOST_SOURCE}/libs/stacktrace/src/addr2line.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/backtrace.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/basic.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/noop.cpp
     ${BOOST_SOURCE}/libs/stacktrace/src/windbg.cpp
-    ${BOOST_SOURCE}/libs/stacktrace/src/windbg_cached.cpp
+#    ${BOOST_SOURCE}/libs/stacktrace/src/windbg_cached.cpp
   DEFINE_PRIVATE
     BOOST_STACKTRACE_STATIC_LINK=1
 )
